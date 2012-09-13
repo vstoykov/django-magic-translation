1c9524b632be01a8b817e1eac8a70b65387d4382
Venelin Stoykov <vkstoykov@gmail.com>
2012-09-13 17:27:23 +0300

    Update setup.py
    Use new README.rst for the long_description

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ install_requires = [
 
 setup(
     name="django-magic-translation",
-    version='0.1.0',
+    version='0.1.1',
     author="Venelin Stoykov",
     author_email="vkstoykov@gmail.com",
     description="Another application that try to make Django Framework translatable"
                 " to different langues easier.",
-    long_description=open("README.md").read(),
+    long_description=open("README.rst").read(),
     url="https://github.com/vstoykov/django-magic-translation",
     zip_safe=False,
     include_package_data=True,
